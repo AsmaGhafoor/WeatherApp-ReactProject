e0e0998f7e2ac6416a641b86b94889fda9c42c31
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2024-01-02 18:53:38 +0500

    React

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import { useState } from 'react';
 
 
 function App() {
-  const API_KEY = "fad41a082a3d6d7ea184e725e4c69ef3";//remove key when pushing to git
+  const API_KEY = "";//remove key when pushing to git
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
   const fetchWeather = async (location) => {
     try {
-      //ja jab kahin string mein ${variableNAme} ho to use backtick(``) push krne se pehle remove krdiyega
       const responce = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`);
       setWeatherData(responce.data);
       setError(null);
